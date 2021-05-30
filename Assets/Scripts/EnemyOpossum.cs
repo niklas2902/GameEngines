d5f3cb7c4611620d67975553ab75164cf3759a47
Lucas Engelbrecht <lengelbr@stud.hs-heilbronn.de>
2021-05-30 16:30:57 +0200

    Added opossum enemy with animation and movement script

---
+++ Assets/Scripts/EnemyOpossum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOpossum : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private float direction = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(direction, 0f, 0f);
+        if(movement.x > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Obstacles")
+        {
+            direction = direction == -1 ? 1 : -1;
+        }
+    }
+}
