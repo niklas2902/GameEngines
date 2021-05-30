dade2dca1f4d08d3eafea4a49649e8f8c5b728d5
Niklas Zimmer <niklaszimmer2902@gmail.com>
2021-05-30 17:44:47 +0200

    Set up basic player behavior

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+
+    public CharacterController controller;
+    
+    private Animator animator;
+
+    bool jump;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("IsJumping", true);
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false,jump);
+        jump = false;
+    }
+
+    public void StopJump()
+    {
+        Debug.Log("StopJump");
+        animator.SetBool("IsJumping", false);
+    }
+}
