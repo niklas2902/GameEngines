5f0e3f51b78ce79acc80b6712abc2ff2323f9d51
Lucas Engelbrecht <lengelbr@stud.hs-heilbronn.de>
2021-06-13 22:01:03 +0200

    Added alternative eagle with ai script

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 400f;
+    public float nextWaypointDistance = 1.2f;
+    public Transform enemyGX;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating(nameof(UpdatePath), 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (rb.velocity.x >= 0.01f)
+        {
+            enemyGX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rb.velocity.x <= 0.01f)
+        {
+            enemyGX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+        
+    }
+}
